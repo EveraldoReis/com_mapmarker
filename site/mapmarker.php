a88e90617e6e07612f04745cf899a47dfc7fe74b
root <root@localhost.localdomain>
2014-12-16 17:09:25 -0200

    Initial commit

---
+++ site/mapmarker.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$doc = JFactory::getDocument();
+
+$doc->addScript('//maps.google.com/maps/api/js?sensor=true');
